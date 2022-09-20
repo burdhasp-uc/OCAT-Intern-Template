d1087154489773c4b14a30b98df7d3260e92daee
Srishant Burdhan <burdhasp@mail.uc.edu>
2022-09-20 16:38:44 -0400

    Task 1 Changes

--- API/src/routes/Assessment/index.js
+++ API/src/routes/Assessment/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable require-await */
+// eslint-disable-next-line no-unused-vars
 const { AssessmentService } = require(`../../microservices`);
 const { ResponseHandler } = require(`../../utils`);
 
@@ -10,7 +14,8 @@ module.exports = server => {
     async (req, res, next) => {
       try {
         const { assessment } = req.params;
-
+        console.log(assessment);
+        AssessmentService.submit(assessment);
         // verify that your data is making it here to the API by using console.log(assessment);
         // call the AssessmentService.submit function from the API/src/microservices/Assessment/ and
         // supply the correct parameters
--- OCAT/client/services/AssessmentService.js
+++ OCAT/client/services/AssessmentService.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 import Axios from '../utils/http.config';
 
 export class AssessmentService {
-  // eslint-disable-next-line no-unused-vars
   static submit(assessment) {
     try {
       // in a request to the express OCAT/server/routes/Assessment/index.js
       // NOTE: the http.config file automatically adds /api to the front of your url
+
       return Axios.post(`/api/assessment/submit`, { assessment })
         .then(response => response.data);
     }
@@ -15,6 +15,15 @@ export class AssessmentService {
       throw new Error(`${err.response.statusText} - ${err.response.data.message}`);
     }
   }
+
+  // const BASE_URL = "/api/assessment/submit";
+  // export const saveAssessment = async (assessment) => {
+  //  try {
+  //    const response = await axios.post(BASE_URL, assessment);
+  //  } catch (err) {
+  //    console.log(err);
+  //  }
+  // };
 
   static getList() {
     try {
