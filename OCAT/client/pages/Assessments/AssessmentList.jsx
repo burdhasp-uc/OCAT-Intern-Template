a4f4c483830f67a1505696ec3b62b1ce67e635aa
Srishant Burdhan <burdhasp@mail.uc.edu>
2022-10-03 09:52:33 -0400

    Task 2 changes

--- OCAT/client/pages/Assessments/AssessmentList.jsx
+++ OCAT/client/pages/Assessments/AssessmentList.jsx
@@ -1,11 +1,49 @@
 /* eslint-disable no-console */
-import React, { Component, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useTable } from 'react-table';
 import { AssessmentService } from '../../services/AssessmentService';
 
+export function Table({ columns, data }) {
+  const {
+    getTableBodyProps,
+    getTableProps,
+    headerGroups,
+    prepareRow,
+    rows,
+  } = useTable({
+    columns,
+    data,
+  });
+
+  return (
+    <table {...getTableProps()}
+      className="table table-striped table-hover text-center fs-5 table-bordered border-dark col-12">
+      <thead>
+        {headerGroups.map(headerGroup =>
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column =>
+              <th {...column.getHeaderProps()}
+                className="bg-secondary text-white fs-4"
+              >{column.render(`Header`)}</th>)}
+          </tr>)}
+      </thead>
+      <tbody{...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) =>
+                <td {...cell.getCellProps()}>{cell.render(`Cell`)}</td>)}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
 export const AssessmentList = () => {
   const [ assessments, setAssessments ] = useState([]);
-  console.log(`This is the front end:`, assessments);
 
   // fetch all assessments using the AssessmentService.getList function from OCAT/client/services/AssessmentService.js
   useEffect(() => {
@@ -15,105 +53,66 @@ export const AssessmentList = () => {
     fetchAssessments();
   }, [ ]);
 
-  // const columns = useMemo(() => tableColumn, [ ]);
-  // const data = assessments;
-
-  // const {
-  //   getTableBodyProps,
-  //   getTableProps,
-  //   headerGroups,
-  //   prepareRow,
-  //   rows,
-  // } = useTable({
-  //   columns,
-  //   data,
-  // });
-
-  // const tableColumn = [
-  //   {
-  //     Header: `ID`,
-  //     accessor: `id`,
-  //   }, {
-  //     Header: `Score`,
-  //     accessor: `score`,
-  //   }, {
-  //     Header: `Risk Level`,
-  //     accessor: `riskLevel`,
-  //   }, {
-  //     Header: `Name`,
-  //     accessor: `cat_name`,
-  //   }, {
-  //     Header: `Date of Birth`,
-  //     accessor: `cat_date_of_birth`,
-  //   }, {
-  //     Header: `Created at`,
-  //     accessor: `createdAt`,
-  //   },
-  // ];
+  const columns = useMemo(
+    () => [
+      {
+        Header: `Assessment Data`,
+        columns: [
+          {
+            Header: `ID`,
+            accessor: `id`,
+          }, {
+            Header: `Score`,
+            accessor: `score`,
+          }, {
+            Header: `Risk Level`,
+            accessor: `risk_level`,
+          }, {
+            Header: `Name`,
+            accessor: `cat_name`,
+          }, {
+            Header: `Date of Birth`,
+            accessor: `cat_date_of_birth`,
+          }, {
+            Header: `Created at`,
+            accessor: `created_at`,
+          },
+        ],
+      },
+    ],
+  );
 
   return (
 
-    <div className="list-group">
-      <table className="table table-striped table-hover">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Cat Name</th>
-            <th scope="col">Date of Birth</th>
-            <th scope="col">Score</th>
-            <th scope="col">Risk Level</th>
-            <th scope="col">Created At</th>
-          </tr>
-        </thead>
-        <tbody>
-          {assessments && assessments.map(assessment =>
-            <tr key={assessment.id}>
-              <td>{assessment.cat_name}</td>
-              <td>{assessment.cat_date_of_birth}</td>
-              <td>{assessment.score}</td>
-              <td>{assessment.risk_level}</td>
-              <td>{assessment.created_at}</td>
-            </tr>)
-          }
-        </tbody>
-      </table>
+    <div className='Table'>
+      <h1><center>Cat Assessment Table</center></h1>
+      <Table columns={columns} data={assessments} />
     </div>
 
-  // <>
-  //   <table
-  //     {...getTableProps()}
-  //     className="table table-striped table-hover text-center table-bordered border-dark col-12"
-  //   >
-  //     <thead>
-  //       {headerGroups.map((head) =>
-  //         <>
-  //           <tr {...head.getHeaderGroupProps()}>
-  //             {head.headers.map((col) =>
-  //               <>
-  //                 <th
-  //                   {...col.getHeaderProps()}
-  //                   className="bg-secondary text-blue"
-  //                 >
-  //                   {col.render(`Header`)}
-  //                 </th>
-  //               </>)}
-  //           </tr>
-  //         </>)}
-  //     </thead>
-  //     <tbody {...getTableBodyProps()}>
-  //       {rows.map((row) => {
-  //         prepareRow(row);
-  //         return (
-  //           <>
-  //             <tr {...row.getRowProps()}>
-  //               {row.cells.map((cell) =>
-  //                 <td {...cell.getCellProps()}>{cell.render(`Cell`)}</td>)}
-  //             </tr>
-  //           </>
-  //         );
-  //       })}
-  //     </tbody>
-  //   </table>
-  // </>
+  //  <div className="list-group">
+  //     <table className="table table-striped table-hover">
+  //       <thead className="thead-dark">
+  //         <tr>
+  //           <th scope="col">Cat Name</th>
+  //           <th scope="col">Date of Birth</th>
+  //           <th scope="col">Score</th>
+  //           <th scope="col">Risk Level</th>
+  //           <th scope="col">Created At</th>
+  //         </tr>
+  //       </thead>
+  //       <tbody>
+  //         {assessments && assessments.map(assessment =>
+  //           <tr key={assessment.id}>
+  //             <td>{assessment.cat_name}</td>
+  //             <td>{assessment.cat_date_of_birth}</td>
+  //             <td>{assessment.score}</td>
+  //             <td>{assessment.risk_level}</td>
+  //             <td>{assessment.created_at}</td>
+  //           </tr>)
+  //         }
+  //       </tbody>
+  //     </table>
+  //   </div>
   );
 };
 
